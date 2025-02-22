cf879babee29033e2fdb83d496072c3d9c929524
SaherNadine <149719057+SaherNadine@users.noreply.github.com>
2025-02-22 22:58:21 +0100

    Update test_dfs.py

--- codes/test_dfs.py
+++ codes/test_dfs.py
@@ -1,6 +1,7 @@
 from DFS import DFS
 import pandas as pd
 import numpy as np
+#from code_3 import Sac_a_dos_dfs
 
 #here u can the change the path to the path of the dataset
 path = "C:\\Users\\sofia\\OneDrive\\Bureau\\Projet metah\\instances_01_KP\\low-dimensional\\f3_l-d_kp_4_20"
@@ -18,3 +19,6 @@ df = df[1:, :]  # deleting the first line
 
 sol = DFS(20,df)
 sol.find_solution()
+
+#sac_dfs = Sac_a_dos_dfs(df, 878)
+#sac_dfs.affich()
